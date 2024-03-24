d69945fba07a5a613793db2c067ff804ea05ea10
NekruzAsh <nekruzashrapov16@gmail.com>
2024-03-24 18:09:11 -0400

    updated the club research section with new dividers and content flex design.

--- app/components/ClubResearch.jsx
+++ app/components/ClubResearch.jsx
@@ -2,29 +2,34 @@ import React from "react";
 
 const ClubResearch = () => {
   return (
-    <div className="w-full h-screen text-center">
+    <div id="clubresearch" className="w-full h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto p-4 flex justify-center items-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum." Section 1.10.32 of "de Finibus Bonorum et
-        Malorum", written by Cicero in 45 BC "Sed ut perspiciatis unde omnis
-        iste natus error sit voluptatem accusantium doloremque laudantium, totam
-        rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
-        quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
-        magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro
-        quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
-        adipisci velit, sed quia non numquam eius modi tempora incidunt ut
-        labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
-        veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
-        nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure
-        reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-        consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
-        pariatur?"
+        <div className="flex flex-col w-full">
+          <div className="mt-48">
+            <p className="text-[#FF204E]">From members of the club</p>
+          <h2 className="text-[#cfcfcf] mt-2">
+            Club Research
+          </h2>
+        <p className="text-[#707173] text-lg mt-2">
+          Here is some of the research that our club members have been working
+        </p>
+        </div>
+          <div className="mt-12 grid h-20 card bg-[#191a1e] rounded-box place-items-center">
+            content
+          </div>
+          <div className="divider"></div>
+          <div className="grid h-20 card bg-[#191a1e] rounded-box place-items-center">
+            content
+          </div>
+          <div className="divider"></div>
+          <div className="grid h-20 card bg-[#191a1e] rounded-box place-items-center">
+            content
+          </div>
+          <div className="divider"></div>
+          <div className="grid h-20 card bg-[#191a1e] rounded-box place-items-center">
+            content
+          </div>
+        </div>
       </div>
     </div>
   );
