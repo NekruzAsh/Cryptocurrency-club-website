3b8ac0b7efda5540dda5558f5afaa55937019f0c
NekruzAsh <nekruzashrapov16@gmail.com>
2024-05-18 21:03:04 -0400

    added an about section about web3

---
+++ app/components/Web3.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Web3 = () => {
+  return (
+    <div className="w-full flex justify-center items-center">
+      <div className="max-w-[1240px] mx-auto items-center text-center">
+        <div className="px-24 pt-10">
+          <h2 className="text-[#cfcfcf]">
+            {" "}
+            <span className="text-[#FF204E]">//</span> What is Web3 and
+            Blockchain?
+          </h2>
+          <p className="text-lg text-[#707173]">
+            Web3 is like the next evolution of the internet, focusing on
+            decentralization and giving individuals more control over their
+            digital lives. Blockchain, on the other hand, is the technology that
+            underpins cryptocurrencies like Bitcoin, providing a secure and
+            transparent digital ledger.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Web3;
